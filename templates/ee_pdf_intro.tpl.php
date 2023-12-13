f6de2ae9fc302a08c1f4dccf166f170b6cbf1b34
OIOIOOOI <anatolii.mn@gmail.com>
2023-12-13 12:56:17 +0200

    EASYOPAC-1486 - Intro page columns size.

--- templates/ee_pdf_intro.tpl.php
+++ templates/ee_pdf_intro.tpl.php
@@ -7,9 +7,9 @@
 ?>
 <table>
   <tr>
-    <td style="width: 20%; height: 22cm;"></td>
-    <td style="width: 40%; text-align: center;"><?php print $title; ?></td>
-    <td style="width: 20%;"></td>
+    <td style="width: 25%; height: 22cm;"></td>
+    <td style="width: 50%; text-align: center;"><?php print $title; ?></td>
+    <td style="width: 25%;"></td>
   </tr>
   <tr>
     <td colspan="3"><?php print $intro; ?></td>
